7fe1700d596c340b0a77f51c87dcb673ad4a0c5c
ellie <ellie.tms1371@gmail.com>
2020-08-08 21:15:46 +0100

    add the description of styled-library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from '../src/Person';
 
 
@@ -54,10 +53,10 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'yellow',
-        color: 'black'
-      }
+      // ':hover': {
+      //   backgroundColor: 'yellow',
+      //   color: 'black'
+      // }
     };
 
     let persons = null;
@@ -92,7 +91,6 @@ class App extends Component {
     }
 
     return (
-       <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join( ' ' )}>This is really working!</p>
@@ -101,10 +99,9 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-       </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium( App );
+export default  App ;
